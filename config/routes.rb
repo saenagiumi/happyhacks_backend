4bfe7ae154574044bdf83f76d01c643641fe9043
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-11 17:07:30 +0900

    [Add]: posts/:post_id/commentsのルーティングを実装した #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
+  # resources :comments
+  resources :posts do
+    resources :comments
+  end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
