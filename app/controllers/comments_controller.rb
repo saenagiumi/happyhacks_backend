7923caeb1b4b2344c8e72350dc25b6e59442dd71
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-11 17:09:22 +0900

    [Add]: post_idに対応したコメント一覧を取得できるように条件を変更した #2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
-    @comments = Comment.all
+    @comments = Comment.where("post_id = ?", params[:post_id])
 
     render json: @comments
   end
