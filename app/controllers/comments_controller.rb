95ae764fc336ef9f273b43596a19b7f9e929c2cd
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-12 10:56:10 +0900

    [Fix]: 単数系だと正しいURLに遷移できないため、複数形に修正した #2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
 
     if @comment.save
-      render json: @comment, status: :created, location: @comment
+      render json: @comments, status: :created, location: @comments
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
