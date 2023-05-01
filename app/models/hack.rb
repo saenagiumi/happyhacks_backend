68e45d72f2285d7eb8114944e9f0fa560075e403
takeru.dev <takeru.konno.cw`gmail.com>
2023-05-01 14:07:28 +0900

    💡 [Add]: モデルのスキーマ情報を追加した

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: hacks
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  category   :string
+#  tags       :string           default([]), is an Array
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  tweet_id   :string
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_hacks_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Hack < ApplicationRecord
   belongs_to :user
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  comment_id :bigint
+#  hack_id    :bigint
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_likes_on_comment_id  (comment_id)
+#  index_likes_on_hack_id     (hack_id)
+#  index_likes_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (comment_id => comments.id)
+#  fk_rails_...  (hack_id => hacks.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :comment, optional: true
